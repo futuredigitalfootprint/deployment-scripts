face2132b54d4df6f5cda7af0f88df405b607166
nocon <nocon@users.noreply.github.com>
2017-12-13 10:56:11 +0000

    Create go.sh

---
+++ consul/go.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing dependencies..."
+if [ -x "$(command -v apt-get)" ]; then
+  sudo su -s /bin/bash -c 'sleep 30 && apt-get update && apt-get install unzip' root
+else
+  sudo yum update -y
+  sudo yum install -y unzip wget
+fi
+
+echo "Fetching Consul..."
+CONSUL=1.0.0
+cd /tmp
+wget https://releases.hashicorp.com/consul/${CONSUL}/consul_${CONSUL}_linux_amd64.zip -O consul.zip --quiet
+
+echo "Installing Consul..."
+unzip consul.zip >/dev/null
+chmod +x consul
+sudo mv consul /usr/local/bin/consul
+sudo mkdir -p /opt/consul/data
+
+# Read from the file we created
+SERVER_COUNT=$0
+CONSUL_JOIN=$1
+
+# Write the flags to a temporary file
+cat >/tmp/consul_flags << EOF
+CONSUL_FLAGS="-server -bootstrap-expect=${SERVER_COUNT} -join=${CONSUL_JOIN} -data-dir=/opt/consul/data"
+EOF
+
+echo "Creating Systemd daemon config..."
+read -d '' CONSUL_DAEMON_CONFIG <<"EOF"
+[Unit]
+Description=consul agent
+Requires=network-online.target
+After=network-online.target
+
+[Service]
+EnvironmentFile=-/etc/sysconfig/consul
+Restart=on-failure
+ExecStart=/usr/local/bin/consul agent $CONSUL_FLAGS -config-dir=/etc/systemd/system/consul.d
+ExecReload=/bin/kill -HUP $MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "${CONSUL_DAEMON_CONFIG}" >> /tmp/consul.service
+
+echo "Installing Systemd service..."
+sudo mkdir -p /etc/sysconfig
+sudo mkdir -p /etc/systemd/system/consul.d
+sudo chown root:root /tmp/consul.service
+sudo mv /tmp/consul.service /etc/systemd/system/consul.service
+sudo mv /tmp/consul*json /etc/systemd/system/consul.d/ || echo
+sudo chmod 0644 /etc/systemd/system/consul.service
+sudo mv /tmp/consul_flags /etc/sysconfig/consul
+sudo chown root:root /etc/sysconfig/consul
+sudo chmod 0644 /etc/sysconfig/consul
+
+echo "Starting Consul..."
+sudo systemctl enable consul.service
+sudo systemctl start consul
